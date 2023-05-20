2adf178493cfa35e873b634dd0b393d91c7bb352
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-05-20 15:18:11 -0300

    night sky with moon and ufo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import './App.css';
 
+function createMoon() {
+  const nightSky = document.getElementById('night-sky');
+  const moon = document.createElement('div');
+  const randomX = Math.round(Math.random() * 100);
+  const randomY = Math.round(Math.random() * 100);
+  moon.style.setProperty('--randomX', randomX);
+  moon.style.setProperty('--randomY', randomY);
+  moon.className = 'moon';
+  moon.style.width = (20 + (Math.random() * 20)) + 'px'; // Random star size
+  moon.style.height = moon.style.width;
+  moon.style.left = randomX + '%'; // Random horizontal position
+  moon.style.top = randomY + '%'; // Random vertical position
+  nightSky.appendChild(moon);
+}
+
 function createStars() {
   const nightSky = document.getElementById('night-sky');
   const numStars = 100; // Number of stars to create
@@ -16,22 +31,41 @@ function createStars() {
 }
 
 function createShootingStar() {
-  const shootingStar = document.createElement('div');
-  const randomX = Math.round(Math.random() * 10)
+  const randomX = Math.round(Math.random() * 10);
   const randomY = Math.round(Math.random() * 10);
-  console.log(randomX + " and " + randomY);
-  shootingStar.style.setProperty('--randomX', randomX);
-  shootingStar.style.setProperty('--randomY', randomY);
-  shootingStar.style.width = ((Math.random() * 2) + 1) + 'px'; // Random star size
-  shootingStar.style.height = shootingStar.style.width;
-  shootingStar.style.left = (Math.random() * 100) + '%'; // Random horizontal position
-  shootingStar.style.top = (Math.random() * 100) + '%'; // Random vertical position
-  document.getElementById('night-sky').appendChild(shootingStar);
-  shootingStar.className = 'shooting-star';
-  setTimeout(() => {
-    shootingStar.remove();
-  }, 1000);
+  const size = (randomX + randomY) / 10;
+  if(size !== 0.5) {
+    const shootingStar = document.createElement('div');
+    shootingStar.style.setProperty('--randomX', randomX);
+    shootingStar.style.setProperty('--randomY', randomY);
+    shootingStar.style.width = ((size) + 1) + 'px'; // Random star size
+    shootingStar.style.height = shootingStar.style.width;
+    shootingStar.style.left = (Math.random() * 100) + '%'; // Random horizontal position
+    shootingStar.style.top = (Math.random() * 100) + '%'; // Random vertical position
+    document.getElementById('night-sky').appendChild(shootingStar);
+    shootingStar.className = 'shooting-star';
+    setTimeout(() => {
+      shootingStar.remove();
+    }, 1000);
+  }
+  else {
+    console.log('El numero es: ' + size);
+    const ufo = document.createElement('div');
+    ufo.style.setProperty('--randomX', (randomX * 10));
+    ufo.style.setProperty('--randomY', (randomY * 10));
+    ufo.style.width = (10 + ((size) * 5)) + 'px'; // Random star size
+    ufo.style.height = ufo.style.width;
+    ufo.style.left = (Math.random() * 100) + '%'; // Random horizontal position
+    ufo.style.top = (Math.random() * 100) + '%'; // Random vertical position
+    document.getElementById('night-sky').appendChild(ufo);
+    ufo.className = 'ufo';
+    setTimeout(() => {
+      ufo.remove();
+    }, 5000);
+  }
 }
+
+setTimeout(createMoon, 500);
 
 setTimeout(createStars, 500);
 
