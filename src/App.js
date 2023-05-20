f692d88e030dfc7ca8501767e0bd851c6279d214
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-05-19 23:03:30 -0300

    night sky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+
+function createStars() {
+  const nightSky = document.getElementById('night-sky');
+  const numStars = 100; // Number of stars to create
+
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.width = Math.random() * 2 + 'px'; // Random star size
+    star.style.height = star.style.width;
+    star.style.left = Math.random() * 100 + '%'; // Random horizontal position
+    star.style.top = Math.random() * 100 + '%'; // Random vertical position
+    nightSky.appendChild(star);
+  }
+}
+
+function createShootingStar() {
+  const shootingStar = document.createElement('div');
+  const randomX = Math.random();
+  const randomY = Math.random();
+  shootingStar.style.setProperty('--randomX', randomX);
+  shootingStar.style.setProperty('--randomY', randomY);
+  shootingStar.style.width = Math.random() * 10 + 'px'; // Random star size
+  shootingStar.style.height = shootingStar.style.width;
+  shootingStar.style.left = Math.random() * 100 + '%'; // Random horizontal position
+  shootingStar.style.top = Math.random() * 100 + '%'; // Random vertical position
+  document.getElementById('night-sky').appendChild(shootingStar);
+  shootingStar.className = 'shooting-star';
+  setTimeout(() => {
+    shootingStar.remove();
+  }, 900);
+}
+
+setTimeout(createStars, 2000);
+
+setTimeout(() => {
+  setInterval(createShootingStar, 4000);
+}, 2000);
 
 function App() {
   return (
-    <div className="App">
+    <div id="night-sky">
       <h1>Hola Mundo !</h1>
     </div>
   );
